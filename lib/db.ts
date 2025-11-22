e8cd76eb4d496f9df249499722587de055909961
shivam-181 <shivamkumar060703@gmail.com>
2025-11-22 11:57:44 +0530

    FINAL FIX: Set DataTable props to 'any[]' in wrapper to resolve Vercel prerender type error

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-// Prevents multiple instances of Prisma Client in development
+// Prevents multiple instances of Prisma Client in developments
 export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
